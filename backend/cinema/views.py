f9be6c1169aad8f2869d7d56375c16cabc4507b4
Валера Часовской <valera.chasovskoy@list.ru>
2026-05-31 22:24:03 +0300

    Create base template and movie list page

--- backend/cinema/views.py
+++ backend/cinema/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
+def movie_list(request):
+    movies = Movie.objects.all()
+
+    context = {
+        'movies': movies,
+        'title': 'Афиша'
+    }
+
+    return render(
+        request,
+        'cinema/movie_list.html',
+        context
+    )
