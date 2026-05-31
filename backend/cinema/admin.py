fcd4a4de40398fe038b6e61d31d8fb02cc228cd9
Валера Часовской <valera.chasovskoy@list.ru>
2026-05-31 22:04:03 +0300

    Register models in admin panel

--- backend/cinema/admin.py
+++ backend/cinema/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Genre, Movie, Session, Ticket
 
-# Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Session)
+admin.site.register(Ticket)
