11ee5eb1738cbeb2b700c1df981974ca9b3ae985
Валера Часовской <valera.chasovskoy@list.ru>
2026-06-05 23:23:02 +0300

    Update Design

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 from .models import Profile
 
@@ -13,3 +14,35 @@ class ProfileForm(forms.ModelForm):
             'phone',
             'bio',
         ]
+
+        widgets = {
+            'avatar': forms.FileInput(attrs={
+                'class': 'form-control'
+            }),
+
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+
+            'bio': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5
+            }),
+        }
+
+
+class LoginUserForm(AuthenticationForm):
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите логин'
+        })
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        })
+    )
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import profile, edit_profile
+from .forms import LoginUserForm
 
 
 urlpatterns = [
     path(
         'login/',
         LoginView.as_view(
-            template_name='users/login.html'
+            template_name='users/login.html',
+            authentication_form=LoginUserForm
         ),
         name='login'
     ),
